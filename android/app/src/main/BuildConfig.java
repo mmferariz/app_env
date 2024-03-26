a2aad6b76ea6e446149822bd737c6f14a8ba4da8
Fernando Arizmendi <mferarizmendi@gmail.com>
2024-03-26 10:46:26 -0600

    fix: "gitignore"

---
+++ android/app/src/main/BuildConfig.java
@@ -0,0 +1,9 @@
+package com.app_env.example;  public final class BuildConfig {
+	public static final String BASE_URL = "test hi;"
+
+	public static final String Name = "grefghserfdghtre;"
+
+	public static final int PAss = 65432654;
+
+	public static final String Welcome = "Back;"
+      }
